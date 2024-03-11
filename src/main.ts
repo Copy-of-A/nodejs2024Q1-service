2c3cdbdad58575aac899d0b3e24daff8b1868dcd
Alla Manshina <moralobsession@gmail.com>
2024-03-12 00:20:23 +0400

    feat: swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  const config = new DocumentBuilder()
+    .setTitle('Music Library')
+    .setDescription('The Music Library API description')
+    .setVersion('1.0')
+    .addTag('Music Library')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
